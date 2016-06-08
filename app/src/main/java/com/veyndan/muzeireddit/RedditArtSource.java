98ea900ac43a72958712b86792283f9f0a3bb7f1
veyndan <veyndan89@gmail.com>
2016-06-08 01:16:37 +0100

    Get access token for Reddit

--- app/src/main/java/com/veyndan/muzeireddit/RedditArtSource.java
+++ app/src/main/java/com/veyndan/muzeireddit/RedditArtSource.java
@@ -1,6 +1,15 @@
 package com.veyndan.muzeireddit;
 
+import android.util.Log;
+
 import com.google.android.apps.muzei.api.RemoteMuzeiArtSource;
+
+import rawjava.Authentication;
+import rawjava.Credentials;
+import rawjava.Reddit;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class RedditArtSource extends RemoteMuzeiArtSource {
     private static final String TAG = "veyndan_RedditArtSource";
@@ -12,6 +21,17 @@ public class RedditArtSource extends RemoteMuzeiArtSource {
 
     @Override
     protected void onTryUpdate(int reason) throws RetryException {
+        Credentials credentials = Credentials.create(getResources().openRawResource(R.raw.credentials));
+        Reddit reddit = new Reddit(credentials);
 
+        reddit.authenticate()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Action1<Authentication>() {
+                    @Override
+                    public void call(Authentication authentication) {
+                        Log.d(TAG, "call: " + authentication.getAccessToken());
+                    }
+                });
     }
 }
