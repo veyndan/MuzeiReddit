c7d002a9bd1f3374ecfd8b6b6afe49e19852a207
veyndan <veyndan89@gmail.com>
2016-06-08 21:50:59 +0100

    Use retrolambda

--- app/src/main/java/com/veyndan/muzeireddit/RedditArtSource.java
+++ app/src/main/java/com/veyndan/muzeireddit/RedditArtSource.java
@@ -1,24 +1,32 @@
 package com.veyndan.muzeireddit;
 
-import android.util.Log;
+import android.content.Intent;
+import android.net.Uri;
 
+import com.google.android.apps.muzei.api.Artwork;
 import com.google.android.apps.muzei.api.RemoteMuzeiArtSource;
 
 import rawjava.Credentials;
 import rawjava.Reddit;
+import rawjava.Sort;
 import rawjava.model.Link;
-import rawjava.model.Listing;
-import rawjava.model.Thing;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class RedditArtSource extends RemoteMuzeiArtSource {
     private static final String TAG = "veyndan_RedditArtSource";
     private static final String SOURCE_NAME = "RedditArtSource";
 
+    private static final int ROTATE_TIME_MILLIS = 3 * 60 * 60 * 1000; // rotate every 3 hours
+
     public RedditArtSource() {
         super(SOURCE_NAME);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setUserCommands(BUILTIN_COMMAND_ID_NEXT_ARTWORK);
     }
 
     @Override
@@ -26,14 +34,22 @@ public class RedditArtSource extends RemoteMuzeiArtSource {
         Credentials credentials = Credentials.create(getResources().openRawResource(R.raw.credentials));
         Reddit reddit = new Reddit(credentials);
 
-        reddit.subreddit("earthporn")
+        reddit.subreddit("earthporn", Sort.NEW)
+                .map(thing -> thing.data.children)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
-                .subscribe(new Action1<Thing<Listing<Thing<Link>>>>() {
-                    @Override
-                    public void call(Thing<Listing<Thing<Link>>> thing) {
-                        Log.d(TAG, "call: " + thing.data.children.get(1).data.title);
-                    }
+                .subscribe(children -> {
+                    Link link = children.get(2).data;
+                    publishArtwork(new Artwork.Builder()
+                            .title(link.title)
+                            .byline(link.author)
+                            .imageUri(Uri.parse(link.url))
+                            .token(link.id)
+                            .viewIntent(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("https://www.reddit.com" + link.permalink)))
+                            .build());
                 });
+
+        scheduleUpdate(System.currentTimeMillis() + ROTATE_TIME_MILLIS);
     }
 }
